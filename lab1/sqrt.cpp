f601035ee0836cc5b2abbae7caab4d3ff8599365
Karen Caguana <karen@MacBook-Pro.local>
2025-09-11 12:25:58 -0400

    sqrt method

--- lab1/sqrt.cpp
+++ lab1/sqrt.cpp
@@ -5,3 +5,32 @@
  * Description : Square root of a double using Newton's method
  * Pledge      : I pledge my honor that I have abided by the Stevens Honor System.
  ******************************************************************************/
+
+#include <sstream>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+
+using namespace std;
+
+ double sqrt(double num, double epsilon) {
+    if (num < 0) {
+        return numeric_limits<double>::quiet_NaN();
+    } if (num == 0 || num == 1) {
+        return num;
+    }
+
+    double prev = num;
+    double next = (prev + num/prev) / 2.0;
+
+    while (abs(prev - next) <= epsilon) {
+        prev = next;
+        next = (prev + num/prev) / 2.0;
+    }
+
+    return next;
+ }
+
+ int main(int argc, char *argv[]))
+
