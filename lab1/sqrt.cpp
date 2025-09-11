18230f058467b473a60609e9a2fe8b0c85976e89
Karen Caguana <karen@MacBook-Pro.local>
2025-09-11 15:53:45 -0400

    error handling

--- lab1/sqrt.cpp
+++ lab1/sqrt.cpp
@@ -6,9 +6,9 @@
  * Pledge      : I pledge my honor that I have abided by the Stevens Honor System.
  ******************************************************************************/
 
-#include <sstream>
 #include <iostream>
 #include <cmath>
+#include <limits>
 #include <iomanip>
 
 
@@ -24,7 +24,7 @@ using namespace std;
     double prev = num;
     double next = (prev + num/prev) / 2.0;
 
-    while (abs(prev - next) <= epsilon) {
+    while (fabs(prev - next) > epsilon) {
         prev = next;
         next = (prev + num/prev) / 2.0;
     }
@@ -32,5 +32,29 @@ using namespace std;
     return next;
  }
 
- int main(int argc, char *argv[]))
+ int main(int argc, char *argv[]) {
+    //needs 1 or 2 arugements max
+    if (argc < 2 || argc > 3) {
+        cout << "Usage: ./sqrt <value> [epsilon]" <<endl;
+        return 1;
+    }
+    
+    double num = atof(argv[1]);
+    double epsilon = 1e-7;
 
+    if (argc == 3) {
+        try {
+            epsilon = atof(argv[2]);
+        } catch (...) {
+            cout << "Epsilon argument must be a positive double." << endl;
+            return 1;
+        }
+        if (epsilon <= 0) {
+            cout << "Epsilon argument must be a positive double." << endl;
+            return 1;
+        }
+    }
+    double ans = sqrt(num, epsilon);
+    cout << fixed << setprecision(8) << ans << endl;
+    return 0;
+ }
