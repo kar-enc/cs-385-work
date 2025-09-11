19d4aa49c02499d67098e54cc431c342873592e7
Karen Caguana <karen@MacBook-Pro.local>
2025-09-11 11:58:42 -0400

    initial lab1 file

---
+++ lab1/sqrt.cpp
@@ -0,0 +1,7 @@
+/*******************************************************************************
+ * Name        : sqrt.cpp
+ * Author      : Karen Caguana
+ * Date        : 09.10.25
+ * Description : Square root of a double using Newton's method
+ * Pledge      : I pledge my honor that I have abided by the Stevens Honor System.
+ ******************************************************************************/
